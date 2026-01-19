e653b1754ba6570584d0434898b987831ac96c14
Xinzhe Yuan <38831183+X1nzhe@users.noreply.github.com>
2026-01-19 08:23:34 -0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,23 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import react from '@astrojs/react';
+
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), react()],
+    vite: {
+        plugins: [tailwindcss()],
+      },
+    markdown: {
+        syntaxHighlight: "prism",
+        remarkPlugins: [remarkMath],
+        rehypePlugins: [rehypeKatex],
+    },
 });
