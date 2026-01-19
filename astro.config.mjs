44dc212b9a61dbe01563ae5fc7219ec632220685
Xinzhe Yuan <38831183+X1nzhe@users.noreply.github.com>
2026-01-19 09:13:03 -0800

    config domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://tech.currents.one',
     integrations: [mdx(), sitemap(), react()],
     vite: {
         plugins: [tailwindcss()],
